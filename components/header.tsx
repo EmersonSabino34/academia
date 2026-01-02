829ca9943b2bffceb6f542fa30713182310983d1
Emerson-Sabino <168974048+EmersonSabino34@users.noreply.github.com>
2026-01-02 20:57:03 +0000

    Fix formatting in header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -24,7 +24,7 @@ export function Header() {
     { name: "Horários", href: "#schedule" },
     { name: "Treinadores", href: "#trainers" },
     { name: "Contato", href: "#contact" },
-  
+
   ]
 
   return (
